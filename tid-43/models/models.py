3b3c8588b2a9b3a7bc72f6e71c4a722d9aa40afb
Jazzlay <firejazz3@gmail.com>
2023-08-02 16:50:43 +0630

    New

---
+++ tid-43/models/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class tid-43(models.Model):
+#     _name = 'tid-43.tid-43'
+#     _description = 'tid-43.tid-43'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
