ef8d4dbe996928b90bc557085f79caa20de27bb4
Jazzlay <firejazz3@gmail.com>
2023-08-10 16:26:43 +0630

    new

---
+++ tid-48/models/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class tid-48(models.Model):
+#     _name = 'tid-48.tid-48'
+#     _description = 'tid-48.tid-48'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
