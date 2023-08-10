084471fac30e0f60cd9e879288bf952dda1f0d14
Jazzlay <firejazz3@gmail.com>
2023-08-10 15:48:01 +0630

    Stable

--- tid-46/__manifest__.py
+++ tid-46/__manifest__.py
@@ -20,13 +20,15 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'hr'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'data/emailtemplate.xml',
+        'data/data.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
