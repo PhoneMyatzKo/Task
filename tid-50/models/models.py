47a760c3d25abfe671533745dd1f989bb312508f
Jazzlay <firejazz3@gmail.com>
2023-08-14 11:04:44 +0630

    stable

--- tid-50/models/models.py
+++ tid-50/models/models.py
@@ -1,18 +1,13 @@
-# -*- coding: utf-8 -*-
+from odoo import models, api, exceptions
 
-# from odoo import models, fields, api
+class StockReturnPicking(models.TransientModel):
+    _inherit = 'stock.return.picking'
 
-
-# class tid-50(models.Model):
-#     _name = 'tid-50.tid-50'
-#     _description = 'tid-50.tid-50'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    def create_returns(self):
+        for wizard in self:
+            for line in wizard.product_return_moves:
+                if line.quantity > line.move_id.quantity_done:
+                    raise exceptions.ValidationError(
+                        "Returned quantity cannot be greater than the delivered quantity."
+                    )
+        return super(StockReturnPicking, self).create_returns()
