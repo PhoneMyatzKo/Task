9e6e2c466dd5458407b8132e8e4b6a191327090e
Jazzlay <firejazz3@gmail.com>
2023-08-15 15:37:41 +0630

    stable

--- tid-47/models/models.py
+++ tid-47/models/models.py
@@ -1,18 +1,26 @@
-# -*- coding: utf-8 -*-
+from odoo import models, fields
 
-# from odoo import models, fields, api
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
 
+    skip_picking_creation = fields.Boolean(default=False, store=False)
 
-# class tid-47(models.Model):
-#     _name = 'tid-47.tid-47'
-#     _description = 'tid-47.tid-47'
+    def _create_picking(self):
+        if not self.skip_picking_creation:
+            return super()._create_picking()
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    def button_approve(self, force=False):
+        self.skip_picking_creation = True
+        result = super().button_approve(force=force)
+        self.skip_picking_creation = False
+        return result
+    
+    
+    def action_create_invoice(self):
+        self.skip_picking_creation = False
+        self._create_picking()
+        for order in self:
+            for line in order.order_line:
+                line.qty_to_invoice = line.product_qty
+        res = super(PurchaseOrder, self).action_create_invoice()
+        return res
