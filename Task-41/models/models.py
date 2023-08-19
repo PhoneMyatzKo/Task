9862e51c4b4422e98561aa08d8e3f7c2848d0eb8
Jazzlay <firejazz3@gmail.com>
2023-08-19 10:35:08 +0630

    fixed T41

--- Task-41/models/models.py
+++ Task-41/models/models.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
+# # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+# from odoo import models, fields, api
 
 
-class tid41(models.Model):
-    _name = 'Task-41'
-    _description = 'Task ID 41'
+# class tid41(models.Model):
+#     _name = 'Task-41'
+#     _description = 'Task ID 41'
     
-    name = fields.Char()
+#     name = fields.Char()
